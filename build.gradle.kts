833f7e70c1096b2be0f9662715c05dff411e7ce3
Nicholas Yip <nicholasyip@obceit.cc>
2018-10-28 14:30:58 +0000

    Create build settings

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+  scala
+  id("com.github.johnrengelman.shadow") version "4.0.2"
+}
+val scalaCompilerPlugin: Configuration = configurations.create("scalaCompilerPlugin")
+
+tasks.getByName<Wrapper>("wrapper") {
+  gradleVersion = "4.10.2"
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+  jcenter()
+}
+dependencies {
+  implementation("org.scala-lang:scala-library:2.12.7")
+  scalaCompilerPlugin("org.scalamacros:paradise_2.12.7:2.1.1")
+  implementation("org.http4s:http4s-blaze-client_2.12:0.19.0")
+  implementation("org.http4s:http4s-dsl_2.12:0.19.0")
+  implementation("org.http4s:http4s-circe_2.12:0.19.0")
+  implementation("io.circe:circe-generic_2.12:0.10.0")
+
+  testImplementation("org.scalacheck:scalacheck_2.12:1.14.0")
+}
+
+tasks.withType<ScalaCompile> {
+  scalaCompileOptions.additionalParameters =
+      listOf("-Xplugin:" + scalaCompilerPlugin.asPath)
+}
