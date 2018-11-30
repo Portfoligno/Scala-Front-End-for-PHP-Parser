e0558a31ac3dc5f6f611b6353f14177a709eebfb
Nicholas Yip <nicholasyip@obceit.cc>
2018-11-30 22:32:48 +0900

    Revisit dependency scopes and relocated Circe packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,26 @@
 plugins {
   maven
   scala
+  `java-library`
   id("com.github.johnrengelman.shadow") version "4.0.2"
 }
 val scalaCompilerPlugin: Configuration = configurations.create("scalaCompilerPlugin")
 
 tasks.getByName<Wrapper>("wrapper") {
   gradleVersion = "4.10.2"
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+  // Combine dependencies from the 'shadow' configuration instead!
+  configurations = listOf(project.configurations.getByName("shadow"))
+
+  artifacts {
+    add("archives", this@withType)
+  }
+  archiveName = "$baseName-$version.$extension"
+
+  relocate("io.circe", "io.github.portfoligno.php.parser.backend.circe")
+  relocate("org.http4s.circe", "io.github.portfoligno.php.parser.backend.http4s.circe")
 }
 
 java {
@@ -19,19 +33,28 @@ repositories {
   maven("https://jitpack.io")
 }
 dependencies {
-  implementation("org.scala-lang:scala-library:2.12.7")
   scalaCompilerPlugin("org.scalamacros:paradise_2.12.7:2.1.1")
+  api("org.scala-lang:scala-library:2.12.7")
+  api("co.fs2:fs2-io_2.12:1.0.0")
+
   implementation("org.http4s:http4s-blaze-client_2.12:0.19.0")
   implementation("org.http4s:http4s-dsl_2.12:0.19.0")
-  implementation("org.http4s:http4s-circe_2.12:0.19.0") {
-    exclude("io.circe")
-  }
-  implementation("io.circe:circe-fs2_2.12:0.10.0") {
-    exclude("io.circe")
-  }
-  implementation("io.github.sd-yip.circe:circe-jawn_2.12:d0923ca4")
-  implementation("io.github.sd-yip.circe:circe-generic-extras_2.12:d0923ca4")
   implementation("is.cir:ciris-cats-effect_2.12:0.11.0")
+
+  shadow("org.http4s:http4s-circe_2.12:0.19.0") { setTransitive(false) }
+  shadow("io.circe:circe-fs2_2.12:0.10.0") { setTransitive(false) }
+  shadow("io.github.sd-yip.circe:circe-jawn_2.12:d0923ca4") { setTransitive(false) }
+  implementation("org.http4s:http4s-jawn_2.12:0.19.0")
+  implementation("org.spire-math:jawn-parser_2.12:0.13.0")
+
+  shadow("io.github.sd-yip.circe:circe-generic-extras_2.12:d0923ca4") { setTransitive(false) }
+  shadow("io.github.sd-yip.circe:circe-generic_2.12:d0923ca4") { setTransitive(false) }
+  implementation("com.chuusai:shapeless_2.12:2.3.3")
+
+  shadow("io.github.sd-yip.circe:circe-core_2.12:d0923ca4") {
+    exclude("org.scala-lang")
+    exclude("org.typelevel")
+  }
 
   testImplementation("org.scalacheck:scalacheck_2.12:1.14.0")
   testImplementation("org.slf4j:slf4j-simple:1.7.25")
